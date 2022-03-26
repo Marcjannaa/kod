04bc07c3072770b45b087a2951dcb596b05328e4
marci <marci@DESKTOP-4FTK8R9>
2022-03-26 13:15:55 +0100

    Dodaj pliki projektów.

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int liczba;
+    int liczba_pobrana = -1;
+    int licznik = 0;
+    srand(time(NULL));
+    liczba = rand() % 100;
+
+    while (liczba != liczba_pobrana)
+    {
+        cin >> liczba_pobrana;
+        if (liczba_pobrana == liczba)
+        {
+            cout << "zgadleś\n";
+        }
+        else
+        {
+            cout << "nie zgadleś";
+            if (liczba_pobrana > liczba)
+                cout << "za wysoko\n";
+            else
+                cout << "za malo\n";
+        }
+        licznik++;
+    }
+    cout << "ilosc prob: " << licznik;
+}
